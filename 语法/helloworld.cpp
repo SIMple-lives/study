48a356bba9310208e5e2426b7577096efe0757c2
future <141832850+SIMple-lives@users.noreply.github.com>
2024-01-30 19:28:35 +0800

    c++

---
+++ 语法/helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+/* 多行注释 */
+// 单行注释
+  
+int main()
+{
+    // 在屏幕中输出helloworld
+    cout <<"helloworld!" <<endl;
+
+    system("pause");
+
+    return 0;
+}
