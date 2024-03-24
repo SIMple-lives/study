cd1dda63415e00d7f57bebac711baa17b2ed2466
SIMple-lives <1305969480@qq.com>
2024-03-24 22:34:47 +0800

    add calculate_time

--- pthread/Athread.cpp
+++ pthread/Athread.cpp
@@ -8,6 +8,25 @@
 #include <future>
 #include <atomic>
 #include <boost/multiprecision/cpp_int.hpp>
+#include <chrono>
+
+class calculate_time
+{
+public:
+    calculate_time()
+    {
+        start = std::chrono::steady_clock::now();
+    }
+    ~calculate_time()
+    {
+        end = std::chrono::steady_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        std::cout << "持续的时间为: " << duration.count() << " microseconds" << std::endl;
+    }
+private:
+    std::chrono::time_point<std::chrono::steady_clock> start;
+    std::chrono::time_point<std::chrono::steady_clock> end;
+};
 
 class TaskQueue 
 {
@@ -99,6 +118,35 @@ public:
         return res;
     }
 
+    void AdjustThreads()
+    {
+        if(m_shutdown) return ;
+        int currentTask = m_task.size();
+        int currentThread = m_threads.size();
+        if(currentTask > currentThread && currentTask < m_maxthreads.load())
+        {
+            int newThread = std::min(currentTask, m_maxthreads.load());
+            int addThread = newThread - currentThread;
+            for(int i=0;i<addThread;i++)
+            {
+                m_threads.emplace_back(ThreadWork(this,currentThread+i));
+            }
+        }
+        else if(currentThread > m_minthreads.load() && currentThread/2>currentTask)
+        {
+            int newthread=std::max(currentTask, m_minthreads.load());
+            int deleteThread = currentThread - newthread;
+            for(int i=0;i< deleteThread;i++)
+            {
+                int lastindex = m_threads.size()-1;
+                if(m_threads.at(lastindex).joinable())
+                {
+                    m_threads.at(lastindex).join();
+                }
+                m_threads.pop_back();
+            }
+        }
+    }
 private:
     class ThreadWork 
     {
@@ -133,6 +181,9 @@ private:
     std::condition_variable m_condition;
     std::mutex m_mutex;
     TaskQueue m_task;
+    std::atomic<bool> m_needAdjust;
+    std::atomic<int> m_maxthreads;
+    std::atomic<int> m_minthreads;
 };
 
 boost::multiprecision::cpp_int factorial(int n) 
@@ -142,6 +193,7 @@ boost::multiprecision::cpp_int factorial(int n)
     {
         result *= i;
     }
+    //std::cout << "Thread Id " << std::this_thread::get_id() << "\t(" <<n << ")" << result << std::endl;
     std::cout << "Thread Id " << std::this_thread::get_id() << "\t";
     return result;
 }
@@ -160,6 +212,7 @@ void task()
         std::cout << "请输入第" << i+1 << "个数字: " << std::endl;
         std::cin >> Num[i];
     }
+    calculate_time time;
     for(size_t i=0;i<Num.size();i++)
     {
         std::function<boost::multiprecision::cpp_int(int)> func=std::bind(factorial,Num[i]);
@@ -169,8 +222,37 @@ void task()
     pool.shutdown();
 }
 
+boost::multiprecision::cpp_int factorial1(int n) 
+{
+    boost::multiprecision::cpp_int result = 1;
+    for (int i = 1; i <= n; ++i) 
+    {
+        result *= i;
+    }
+    return result;
+}
+
+void task1()
+{
+    int n_Num;
+    std::cout << "请输入需要计算的数量: " << std::endl;
+    std::cin >> n_Num;
+    std::vector<int> Num(n_Num);
+    for(int i=0;i<n_Num;i++)
+    {
+        std::cout << "请输入第" << i+1 << "个数字: " << std::endl;
+        std::cin >> Num[i];
+    }
+    calculate_time time;
+    for(size_t i=0;i<Num.size();i++)
+    {
+        std::cout << "result(" << Num[i] << ") = " << factorial1(Num[i]) << std::endl;
+    }
+}
+
 int main() 
 {
     task();
+    task1();
     return 0;
 }
