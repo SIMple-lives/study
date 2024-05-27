0d653f234b04d070b602bde46b8a409449693642
SIMple-lives <1305969480@qq.com>
2024-05-27 22:52:09 +0800

    add define

---
+++ Chatroom/head/define.hpp
@@ -0,0 +1,94 @@
+#define REQUEST "request"
+#define SUCCESS 1
+#define FAILURE -1
+
+//状态
+#define EXIST 3
+#define NEXIST 5
+
+#define OFFLINE 12
+#define ONLINE 13
+
+#define ISFRIEND 17
+#define ISNFRIEND 19
+
+#define PASSWORDERROR 9
+
+#define ALREADYLOGIN 14
+
+#define ISBLOCKFRIEND 22
+#define ISNBLOCKFRIEND 25
+
+#define NOFRIEND 28
+
+#define NOBLOCKFRIEND 30
+
+#define NOOFFLINEMSG 40
+
+//好友状态
+#define BLOCK 23
+#define FRIEND 24
+
+//好友申请状态
+#define REFUSE 34
+#define ACCEPT 35
+#define IGNORE 36
+#define NOFRIENDREQUSET 37
+
+//请求
+#define LOGOUT 6
+#define SIGNUP 2
+#define FINDPASSWORDSA 7
+#define FINDPASSWORDSB 8
+#define CHAT 10
+#define EXIT 11
+#define ADDFRIEND 16
+#define FRIENDINFO 18 
+#define PERSONALINFO 20
+#define BLOCKFRIEND 21
+#define USEREXIT 15
+#define LOGIN 4
+#define VIEWFRIENDS 26
+#define DELEFRIEND 27
+#define VIEWBLOCKFRIENDS 29
+#define VIEWFRIENDSREQUSET 32
+#define HANDLEFRIENDREQUSET 33
+#define PRIVATECHAT 38
+#define UNBLOCKFRIEND 39
+#define EXITCHAT 41
+#define REFRESH 42
+#define CREATEGROUP 44
+#define JOINGROUP 46
+#define QUITGROUP 53
+#define LISTGROUP 56
+#define MEMBERGROUP 59
+#define DELEGROUP 60
+#define HANDLEGROUPREQUEST 62
+#define DOGROUPREQUEST 63
+//消息状态
+#define FRIENDREQUEST 31
+
+//无消息
+#define NOSIGN 43
+
+
+//空格
+#define SPACE "\t\b\b\b\b\b"
+
+//群聊状态
+#define GROUPNAMEEXIST 45
+#define GROUPIDNOTEXIST 47
+#define JOINOWNGROUP 48
+#define ALREADYJOIN 50
+
+#define OKSPEAK 51
+#define NOTSPEAK 52
+
+
+#define ISME 49
+
+#define NOTINGROUP 54
+#define NOTQUIT 57
+
+#define NOGROUP 58
+#define NOTGROUPOWNER 61
